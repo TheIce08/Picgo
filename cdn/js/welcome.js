9c3ca509b1a4d533b503f3ecb0822982ac27bac1
TheIce08 <142497590+TheIce08@users.noreply.github.com>
2023-10-13 20:32:41 +0800

    Update welcome.js

--- cdn/js/welcome.js
+++ cdn/js/welcome.js
@@ -26,7 +26,7 @@ function getDistance(e1, n1, e2, n2) {
 
 function showWelcome() {
 
-    let dist = getDistance(120.4448, 31.4047, ipLocation.data.lng, ipLocation.data.lat); //修改自己的经度（121.413921）纬度（31.089290）
+    let dist = getDistance(120.75950,31.65954, ipLocation.data.lng, ipLocation.data.lat); //修改自己的经度（121.413921）纬度（31.089290）
     let pos = ipLocation.data.country;
     let ip = ipLocation.ip;
     let posdesc;
